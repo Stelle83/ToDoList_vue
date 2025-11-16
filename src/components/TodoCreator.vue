014d2af5c775234c15ddf919e03e3747cf7e06b6
Stelle83 <stelle83@gmail.com>
2025-11-16 23:36:49 +0100

    add field to create tasks

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -1,11 +1,33 @@
 <script setup>
+import { reactive, defineEmits } from "vue";
+
+const emit = defineEmits(["create-todo"]);
+
+const todoState = reactive({
+    todo: "",
+    invalid: null,
+    errMsg: "",
+});
+
+const createTodo = () => {
+    todoState.invalid = null;
+    if (todoState.todo !== "") {
+        emit("create-todo", todoState.todo);
+        todoState.todo = "";
+        return;
+    }
+    todoState.invalid = true;
+    todoState.errMsg = "Task cannot be empty!";
+};
 </script>
 
 <template>
-    <div class="input-wrap">
-        <input type="text" placeholder="Enter your task here..." />
-        <button>Add Task</button>
+    <div class="input-wrap" :class="{ 'input-err': todoState.invalid }">
+        <input type="text" placeholder="Enter your task here..." v-model="todoState.todo" />
+        <button @click="createTodo()">Add Task</button>
     </div>
+<p v-show="todoState.invalid" class="err-msg"> {{ todoState.errMsg }}</p>
+
 </template>
 
 <style scoped>
@@ -13,6 +35,11 @@
         display: flex;
         transition: 250ms ease;
         border: 2px solid orange;
+
+        &.input-err {
+            border-color: red;
+        }
+
         &:focus-within {
           /*  box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
             0 -2px 4px -2px rgb(0 0 0 / 0.1); */
@@ -39,4 +66,11 @@
             background-color: darkorange;
         }
     }
+
+    .err-msg {
+        color: red;
+        margin-top: 6px;
+        font-size: 12px;
+        text-align: center;
+    }   
 </style>
